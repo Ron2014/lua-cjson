5a13954f18596b62c82fb5460b79ff6cf6d09ce8
Ron <developer.ron.jim@gmail.com>
2019-12-05 01:50:36 +0800

    LUALIB_API & dllexport

--- lua_cjson.c
+++ lua_cjson.c
@@ -1400,7 +1400,7 @@ static int lua_cjson_safe_new(lua_State *l)
     return 1;
 }
 
-int luaopen_cjson(lua_State *l)
+LUALIB_API int luaopen_cjson(lua_State *l)
 {
     lua_cjson_new(l);
 
@@ -1414,7 +1414,7 @@ int luaopen_cjson(lua_State *l)
     return 1;
 }
 
-int luaopen_cjson_safe(lua_State *l)
+LUALIB_API int luaopen_cjson_safe(lua_State *l)
 {
     lua_cjson_safe_new(l);
 
--- lua_cjson.h
+++ lua_cjson.h
@@ -5,7 +5,8 @@
 extern "C" {
 #endif
 
-int luaopen_cjson(lua_State *l);
+LUALIB_API int luaopen_cjson(lua_State *l);
+LUALIB_API int luaopen_cjson_safe(lua_State *l);
 
 #ifdef __cplusplus
 }
